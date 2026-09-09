1ad62bc5eeadea03ce73deaf41f56b3bcda87096
jeevan <mvjeevan1@gmail.com>
2026-09-09 16:22:59 +0530

    commit 2"

---
+++ middle of the linked list.c
@@ -0,0 +1,9 @@
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode*ptr1=head;
+    struct ListNode*ptr2=head;
+    while(ptr2!=NULL&&ptr2->next!=NULL){
+        ptr1=ptr1->next;
+        ptr2=ptr2->next->next;
+    }
+    return ptr1;
+}
